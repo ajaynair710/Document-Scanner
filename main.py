64fbb48337e673b94cb266b992493c4a8fc46e06
Ajay Prakash Nair <42165927+ajaynair710@users.noreply.github.com>
2020-06-08 12:27:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import cv2
 import numpy as np
 
-frameWidth = 640
-frameHeight = 480
+
+widthImg = 640
+heightImg = 480
+
+
+
 cap=cv2.VideoCapture(0)
-cap.set(3,frameWidth)
-cap.set(4,frameHeight)
+cap.set(3,widthImg)
+cap.set(4,heightImg)
 cap.set(10,150)
+
+def preProcessing(img):
+    imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray,(5,5),1)
+    imgCanny = cv2.Canny(imgBlur,200,200)
+    kernel = np.ones((5,5))
+    imgDilation = cv2.dilate(imgCanny,kernel,iterations=2)
+    imgThreshold=cv2.erode(imgDilation,kernel,iterations=1)
+    return imgThreshold
+
+
 
 while True:
     success,img=cap.read()
-    cv2.imshow("Result", img)
+    img=cv2.resize(img,(widthImg, heightImg))
+    imgThreshold=preProcessing(img)
+    cv2.imshow("Result", imgThreshold)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
