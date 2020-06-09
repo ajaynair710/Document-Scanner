4f0c1638b7acaf107c9d4355ed0d9f797537516a
Ajay Prakash Nair <42165927+ajaynair710@users.noreply.github.com>
2020-06-09 10:50:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,11 @@ import cv2
 import numpy as np
 
 
-widthImg = 640
-heightImg = 480
+widthImg=540
+heightImg =640
 
 
-
-cap=cv2.VideoCapture(0)
-cap.set(3,widthImg)
-cap.set(4,heightImg)
+cap = cv2.VideoCapture(0)
 cap.set(10,150)
 
 def preProcessing(img):
@@ -17,10 +14,9 @@ def preProcessing(img):
     imgBlur = cv2.GaussianBlur(imgGray,(5,5),1)
     imgCanny = cv2.Canny(imgBlur,200,200)
     kernel = np.ones((5,5))
-    imgDilation = cv2.dilate(imgCanny,kernel,iterations=2)
-    imgThreshold=cv2.erode(imgDilation,kernel,iterations=1)
-    return imgThreshold
-
+    imgDial = cv2.dilate(imgCanny,kernel,iterations=2)
+    imgThres = cv2.erode(imgDial,kernel,iterations=1)
+    return imgThres
 
 def getContours(img):
     biggest = np.array([])
@@ -38,19 +34,18 @@ def getContours(img):
     cv2.drawContours(imgContour, biggest, -1, (255, 0, 0), 20)
     return biggest
 
-
 def reorder (myPoints):
     myPoints = myPoints.reshape((4,2))
     myPointsNew = np.zeros((4,1,2),np.int32)
     add = myPoints.sum(1)
+    #print("add", add)
     myPointsNew[0] = myPoints[np.argmin(add)]
     myPointsNew[3] = myPoints[np.argmax(add)]
     diff = np.diff(myPoints,axis=1)
     myPointsNew[1]= myPoints[np.argmin(diff)]
     myPointsNew[2] = myPoints[np.argmax(diff)]
+    #print("NewPoints",myPointsNew)
     return myPointsNew
-
-
 
 def getWarp(img,biggest):
     biggest = reorder(biggest)
@@ -65,15 +60,57 @@ def getWarp(img,biggest):
     return imgCropped
 
 
-
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
 
 while True:
-    success,img=cap.read()
-    img=cv2.resize(img,(widthImg, heightImg))
+    success, img = cap.read()
+    img = cv2.resize(img,(widthImg,heightImg))
     imgContour = img.copy()
-    imgThreshold=preProcessing(img)
-    biggest = getContours(imgThreshold)
-    cv2.imshow("Result", imgThreshold)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
 
+    imgThres = preProcessing(img)
+    biggest = getContours(imgThres)
+    if biggest.size !=0:
+        imgWarped=getWarp(img,biggest)
+        # imageArray = ([img,imgThres],
+        #           [imgContour,imgWarped])
+        imageArray = ([imgContour, imgWarped])
+        cv2.imshow("ImageWarped", imgWarped)
+    else:
+        # imageArray = ([img, imgThres],
+        #               [img, img])
+        imageArray = ([imgContour, img])
+
+    stackedImages = stackImages(0.6,imageArray)
+    cv2.imshow("WorkFlow", stackedImages)
+
+    if cv2.waitKey(1) and 0xFF == ord('q'):
+        break
